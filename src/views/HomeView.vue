53f3a778c2c4171facf342817eb44cd98ebfc006
James Lanham <james.r.lanham2021@gmail.com>
2023-09-27 15:40:01 -0400

    Clean up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,11 @@
-<script setup lang="ts">
-import HomepageNavCarousel from '../components/HomepageNavCarousel.vue';
-</script>
-
 <template>
   <main>
     <Suspense>
       <HomepageNavCarousel />
     </Suspense>
-    
   </main>
 </template>
+
+<script setup lang="ts">
+  import HomepageNavCarousel from '../components/HomepageNavCarousel.vue';
+</script>
