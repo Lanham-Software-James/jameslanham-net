4c57774092a9c6ec132225f3dd50a4a41230f8af
James Lanham <james.r.lanham2021@gmail.com>
2023-09-27 19:57:00 -0400

    Pulling carousel images from firebase storage instead of locally

--- src/components/HomepageNavCarousel.vue
+++ src/components/HomepageNavCarousel.vue
@@ -26,69 +26,62 @@
     </el-menu>
 
     <el-carousel id="carousel" height="75vh">
-      <el-carousel-item v-for="(image, index) in images" :key="image">
-        <div class="hero" :style="getStyle(index)" />
+      <el-carousel-item v-for="image in images" :key="image">
+        <div class="hero" :style="buildBackground(image)" />
       </el-carousel-item>
     </el-carousel>
   </div>
 </template>
 
-<script lang="ts">
-import { ref } from 'vue'
-import Img1 from '../assets/pexels-lukas-574071.jpg'
-import Img2 from '../assets/pexels-fauxels-3184416.jpg'
-import Img3 from '../assets/pexels-pixabay-416405.jpg'
-
-const activeIndex = ref('1')
-const handleSelect = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
-
-export default {
-  data(){
-    return{
-      images: [Img1, Img2, Img3],
-      activeIndex: '',
-      handleSelect: ''
-    }
-  },
-  methods: {
-    getStyle(index: number): string {
-      return `background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("${ this.images[index] }");`;
-    }
-  }
-}
-
-</script>
-
 <style scoped>
+  #wrapper {
+    position: relative;
+  }
 
-#wrapper {
-  position: relative;
-}
+  #navbar {
+    position:absolute;
+    top:0;
+    z-index: 1;
+    background: rgb(255, 255, 255); opacity: 0.75;
+    width:100%;
+    height: 7.5vh;
+  }
 
-#navbar {
-  position:absolute;
-  top:0;
-  z-index: 1;
-  background: rgb(255, 255, 255); opacity: 0.75;
-  width:100%;
-  height: 7.5vh;
-}
+  #carousel {
+    top:0;
+    z-index: 0;
+  }
 
-#carousel {
-  top:0;
-  z-index: 0;
-}
+  .hero {
+    height:75vh;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
 
-.hero {
-  height:75vh;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-.flex-grow {
-  flex-grow: 1;
-}
+  .flex-grow {
+    flex-grow: 1;
+  }
 </style>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import { homepageNavCarouselStorage } from '../firebaseInit';
+  import { listAll, getDownloadURL } from 'firebase/storage';
+
+  const activeIndex = ref('1')
+  const handleSelect = {key: "", keyPath: []} 
+
+  let images: string[] = [];
+
+  const fileSnap = await listAll(homepageNavCarouselStorage);
+
+  for (const item of fileSnap.items) {
+    const url = await getDownloadURL(item);
+    images.push(url);
+  }
+
+  function buildBackground(image: string): string {
+    return `background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("${ image }");`;
+  }
+</script>
