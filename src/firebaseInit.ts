6e2ed6f18d5fc19cf060b88341f23506ad805d7c
James Lanham <james.r.lanham2021@gmail.com>
2023-09-27 20:14:13 -0400

    Adding firestore.

--- src/firebaseInit.ts
+++ src/firebaseInit.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { collection, getFirestore } from "firebase/firestore";
 import { getStorage, ref as storageRef } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,4 +20,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const storage = getStorage(firebaseApp);
+const firestore = getFirestore(firebaseApp);
+
 export const homepageNavCarouselStorage = storageRef(storage, 'HomepageNavCarousel/');
+export const navigationFirestore = collection(firestore, 'navigation/')
